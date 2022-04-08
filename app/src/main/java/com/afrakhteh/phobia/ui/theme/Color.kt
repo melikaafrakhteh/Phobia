4314df4d5c9b3a2fc075aea305fb1d74647a5fbe
melikaafrakhteh <45101381+melikaafrakhteh@users.noreply.github.com>
2022-04-08 21:38:16 +0430

    Add initialise color and font setup

--- app/src/main/java/com/afrakhteh/phobia/ui/theme/Color.kt
+++ app/src/main/java/com/afrakhteh/phobia/ui/theme/Color.kt
@@ -2,7 +2,6 @@ package com.afrakhteh.phobia.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val black = Color(0xFF212121)
+val blood = Color(0xFF660000)
+val gray = Color(0xFF3E3E3E)
--- app/src/main/java/com/afrakhteh/phobia/ui/theme/Type.kt
+++ app/src/main/java/com/afrakhteh/phobia/ui/theme/Type.kt
@@ -2,27 +2,43 @@ package com.afrakhteh.phobia.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.afrakhteh.phobia.R
 
-// Set of Material typography styles to start with
+
+val mainFontFamily = FontFamily(
+    Font(R.font.robotoregular,FontWeight.Normal),
+    Font(R.font.robotobold, FontWeight.Bold)
+)
+
+val fontFamily = FontFamily(
+    Font(R.font.horroroid, FontWeight.Normal),
+    Font(R.font.horroroidbold, FontWeight.Bold)
+)
+
 val Typography = Typography(
+    h1 = TextStyle(
+        fontFamily = fontFamily,
+        fontSize = 30.sp,
+    ),
+    h2 = TextStyle(
+        fontFamily = mainFontFamily,
+        fontSize = 24.sp,
+    ),
+    h3 = TextStyle(
+        fontFamily = fontFamily,
+        fontSize = 18.sp,
+    ),
+    button = TextStyle(
+        fontFamily = mainFontFamily,
+        fontSize = 16.sp
+    ),
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontFamily = mainFontFamily,
         fontSize = 16.sp
     )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
-    ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
-    )
-    */
 )
